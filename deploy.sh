561fda3acd34f96d4aacede2af84a82f7c226b06
Roy Ronalds <tchalvakspam@gmail.com>
2017-11-20 21:02:31 -0500

    Index: Update the logo image, move deploy.sh, add a few nw images.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "...syncing..."
-aws s3 sync ./ s3://ninjawars-splash/ --profile=bitdog --exclude ".git/*" --exclude "deploy.sh" --exclude "README.md" --delete
+aws s3 sync ./www/ s3://ninjawars-splash/ --profile=bitdog --exclude ".git/*" --exclude "deploy.sh" --exclude "README.md" --delete
 echo "---------------------------------------------------------------------------------------"
 echo ""
 echo "Site should be uploaded at: http://ninjawars-splash.s3-website-us-east-1.amazonaws.com"
