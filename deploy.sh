d97a2d6b1666ccba591078fe5207f4181dc56a65
Roy Ronalds <roy.ronalds@gmail.com>
2018-07-14 09:49:55 -0400

    Update deploy and error pages.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ echo "...syncing..."
 aws s3 sync ./www/ s3://ninjawars-splash/ --profile=bitdog --exclude ".git/*" --exclude "deploy.sh" --exclude "README.md" --delete
 echo "---------------------------------------------------------------------------------------"
 echo ""
-echo "Site should be uploaded at: http://ninjawars-splash.s3-website-us-east-1.amazonaws.com"
+echo "Site should be uploaded at: https://splash.ninjawars.net"
 echo "Done."
